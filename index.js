e03cb3a35020dcf204929799a9b5990b9b4cdb14
Rayane Benamre <benamrerayane@proton.me>
2024-01-23 00:31:45 +0100

    finishedd

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-import Fastify from "fastify";
-const fastify = Fastify({
-  logger: true,
+const path = require("path");
+const fastify = require("fastify")();
+
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "public"),
 });
 
-fastify.get("/", function (request, reply) {
-  reply.send({ hello: "world" });
+fastify.get("/", (request, reply) => {
+  reply.send("hi");
+});
+
+fastify.get("/random", (request, reply) => {
+  const option = Math.random();
+  if (option > 0.5) {
+    reply.sendFile("first.mp4");
+  } else {
+    reply.sendFile("second.mp4");
+  }
 });
 
 fastify.listen({ port: 3000 }, function (err, address) {
+  console.log(`Listening on ${address}`);
   if (err) {
     fastify.log.error(err);
     process.exit(1);
